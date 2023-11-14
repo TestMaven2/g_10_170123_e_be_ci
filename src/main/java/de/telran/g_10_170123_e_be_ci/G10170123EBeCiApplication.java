3caaed296ce41140001d6b591e2773d00d2ed8e1
Andrey <k-udm@ya.ru>
2023-11-14 19:06:59 +0100

    Initial commit

---
+++ src/main/java/de/telran/g_10_170123_e_be_ci/G10170123EBeCiApplication.java
@@ -0,0 +1,13 @@
+package de.telran.g_10_170123_e_be_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G10170123EBeCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G10170123EBeCiApplication.class, args);
+	}
+
+}
